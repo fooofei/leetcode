4952877cce84011b535a0554dbc0005f18fcaa77
fooofei <aihujianfei@gmail.com>
2019-09-20 16:38:47 +0800

    add a test case

--- exam-0907/src/main.rs
+++ exam-0907/src/main.rs
@@ -14,6 +14,10 @@
 // [ 1, 0, 5, 7, 8, 7, 6, 4, 3, 3, 1] = 26
 //   2+ 1+ 2+ 3+ 5+  4+ 3+ 2+ 1+ 2+ 1 = 26
 
+// [ 1, 0, 5, 6, 7, 8, 7, 6, 9, 3, 3, 1]
+//   2+ 1+ 2+ 3+ 4+ 5+ 2+ 1+ 2+ 1+ 2+ 1= 26
+
+use std::cmp::max;
 
 fn min_bullet(grades : &Vec<i32>) -> i32 {
 
@@ -29,24 +33,84 @@ fn min_bullet(grades : &Vec<i32>) -> i32 {
     }
     // 反方向，走在前面的比走在后面的多，就是 走在后面的子弹数+1
     for i in (1..grades.len()).rev() {
-        if grades[i-1] > grades[i] {
+        if grades[i-1] > grades[i] && mins[i]+1 >mins[i-1] {
             mins[i-1] = mins[i]+1;
         }
     }
     return mins.iter().sum::<i32>();
-
 }
+
+struct WithIndex {
+    value :i32,
+    index: usize,
+}
+
+// 使用栈的解决方案
+fn min_bullet2(grades : &Vec<i32>) ->i32 {
+
+    let mut dec_stack :Vec<WithIndex>= Vec::new();
+    let mut mins = vec![0;grades.capacity()];
+
+    for i in 0..grades.len(){
+
+        match dec_stack.last() {
+            Some(last) => {
+                let cur = grades[i];
+                if last.value > cur {
+                    dec_stack.push(WithIndex{value:cur, index:i});
+                } else {
+                    let mut bullet = max(1, mins[last.index]);
+                    if last.value < cur{
+                        mins[i] = bullet+1;
+                    } else {
+                        mins[i] = 1;
+                    }
+
+                    while let Some(with_index) = dec_stack.pop(){
+                        mins[with_index.index] = max(bullet, mins[with_index.index]);
+                        bullet +=1 ;
+                    }
+                    dec_stack.push(WithIndex{value:cur,index:i});
+                }
+            },
+            None => {
+                dec_stack.push(WithIndex{value:grades[i],index:i});
+            },
+        }
+    }
+
+    let mut bullet = 1;
+    while let Some(with_index) = dec_stack.pop(){
+        mins[with_index.index] = max(bullet, mins[with_index.index]);
+        bullet += 1;
+    }
+    
+    return mins.iter().sum::<i32>();
+}
+
 
 #[test]
 fn test1(){
+    assert_eq!(min_bullet(&vec![1]), 1);
     assert_eq!(min_bullet(&vec![1,0,5]), 5);
     assert_eq!(min_bullet(&vec![1,2,2]), 4);
     assert_eq!(min_bullet(&vec![ 1, 0, 5, 7, 8, 7, 6, 4, 3, 3, 1] ), 26);
+    assert_eq!(min_bullet(&vec![ 1, 0, 5, 6, 7, 8, 7, 6, 9, 3, 3, 1] ), 26);
 }
+
+#[test]
+fn test2(){
+    assert_eq!(min_bullet2(&vec![1]), 1);
+    assert_eq!(min_bullet2(&vec![1,0,5]), 5);
+    assert_eq!(min_bullet2(&vec![1,2,2]), 4);
+    assert_eq!(min_bullet2(&vec![ 1, 0, 5, 7, 8, 7, 6, 4, 3, 3, 1] ), 26);
+    assert_eq!(min_bullet2(&vec![ 1, 0, 5, 6, 7, 8, 7, 6, 9, 3, 3, 1] ), 26);
+}
+
 
 fn main() {
 
-    println!("{:?}", min_bullet(&vec![1,0,5]));
+    println!("{:?}", min_bullet2(&vec![1,2,2]));
 
     println!("Hello, world!");
 }
