8bddebc311d0e4d5b01a8bf2beb2540c0c6d2c1c
fooofei <aihujianfei@gmail.com>
2019-09-27 10:34:03 +0800

    format

--- cn-084-largest-rectangle/rectangle_segtree.go
+++ cn-084-largest-rectangle/rectangle_segtree.go
@@ -1,6 +1,5 @@
 package leetcode
 
-
 import (
 	"leetcode/segtree"
 	"math"
@@ -9,48 +8,48 @@ import (
 type intSlice []int
 
 func (s intSlice) Len() int {
-return len(s)
+	return len(s)
 }
 
 // Oper ... 实现 Oper，返回最小值的索引
 func (s intSlice) Oper(left int, right int) int {
-if s[left] < s[right] {
-return left
-}
-return right
+	if s[left] < s[right] {
+		return left
+	}
+	return right
 }
 
 func largestRectangleAreaDivisionSegmentTree(heights []int) int {
 
-if len(heights) <= 0 {
-return 0
-}
+	if len(heights) <= 0 {
+		return 0
+	}
 
-st := segtree.New(intSlice(heights))
+	st := segtree.New(intSlice(heights))
 
-return largestRectangleAreaDivisionSt(st, heights, 0, len(heights)-1)
+	return largestRectangleAreaDivisionSt(st, heights, 0, len(heights)-1)
 }
 
 func largestRectangleAreaDivisionSt(st *segtree.SegmentTree, heights []int, start int, end int) int {
 
-idx := 0
-if start < end {
-idx = st.Search(start, end, intSlice(heights))
-} else {
-// 只有 1 个值，不进行搜索了，加速
-idx = start
-}
+	idx := 0
+	if start < end {
+		idx = st.Search(start, end, intSlice(heights))
+	} else {
+		// 只有 1 个值，不进行搜索了，加速
+		idx = start
+	}
 
-minValue := heights[idx]
+	minValue := heights[idx]
 
-areaMax := minValue * (end - start + 1)
-if idx > start {
-areaLeft := largestRectangleAreaDivisionSt(st, heights, start, idx-1)
-areaMax = int(math.Max(float64(areaMax), float64(areaLeft)))
-}
-if idx < end {
-areRight := largestRectangleAreaDivisionSt(st, heights, idx+1, end)
-areaMax = int(math.Max(float64(areaMax), float64(areRight)))
-}
-return areaMax
+	areaMax := minValue * (end - start + 1)
+	if idx > start {
+		areaLeft := largestRectangleAreaDivisionSt(st, heights, start, idx-1)
+		areaMax = int(math.Max(float64(areaMax), float64(areaLeft)))
+	}
+	if idx < end {
+		areRight := largestRectangleAreaDivisionSt(st, heights, idx+1, end)
+		areaMax = int(math.Max(float64(areaMax), float64(areRight)))
+	}
+	return areaMax
 }
