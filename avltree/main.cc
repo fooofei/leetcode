b68eb4e5089a3635908b5847119bfebeb16913d5
h00437043 <hujianfei3@huawei.com>
2019-10-22 21:40:55 +0800

    Update main.cc

--- avltree/main.cc
+++ avltree/main.cc
@@ -20,18 +20,27 @@ struct Node {
     struct Node* left;
     struct Node* right;
     struct Node* parent;
+
+    struct Node(int v)
+        : value(v)
+        , height(0)
+        , left(NULL)
+        , right(NULL)
+        , parent(NULL)
+    {
+    }
 };
 using Node = struct Node;
 
 void treeInsert(Node* n, Node* new_)
 {
     for (; n;) {
-        if (new_->value >= n->value) {
+        if (new_->value > n->value) {
             if (n->right == NULL) {
                 break;
             }
             n = n->right;
-        } else {
+        } else if(new_->value < n->value) {
             if (n->left == NULL) {
                 break;
             }
@@ -50,9 +59,23 @@ void treeInsert(Node* n, Node* new_)
         n->left = new_;
         new_->parent = n;
     }
+    // 需要刷新高度
     if (n->height == 0) {
         for (; n;) {
-            n->height += 1;
+            int left = 0;
+            int right = 0;
+            if (n->left) {
+                left = n->left->height;
+            }
+            if (n->right) {
+                right = n->right->height;
+            }
+            int height = max(left, right) + 1;
+            // 不需要向上刷新了
+            if (height == n->height) {
+                break;
+            }
+            n->height = height;
             n = n->parent;
         }
     }
@@ -60,9 +83,85 @@ void treeInsert(Node* n, Node* new_)
     // banlance
 }
 
+/*
+	   x
+	  /
+	 y
+	/
+   z
+
+
+	y
+   / \
+  z   x
+
+*/
+
+Node* rightRoutate(Node* x)
+{
+    Node* y = x->left;
+    x->left = NULL;
+    y->right = x;
+
+    y->height = max(y->left->height, y->right->height) + 1;
+    return y;
+}
+
+void levelOrderWithNull(const vector<Node*>& childs, vector<string>& vals)
+{
+    vector<Node*> childs2;
+
+    bool haveChilds = false;
+    for (auto it : childs) {
+        vals.push_back(to_string(it->value));
+        if (it->left || it->right) {
+            haveChilds = true;
+        }
+    }
+
+    if (haveChilds) {
+        for (auto it : childs) {
+            if (it->left) {
+                childs2.push_back(it->left);
+            } else {
+                childs2.push_back(new Node(-10000));
+            }
+
+            if (it->right) {
+                childs2.push_back(it->right);
+            } else {
+                childs2.push_back(new Node(-10000));
+            }
+        }
+        levelOrderWithNull(childs2, vals);
+    }
+}
+
+void test1()
+{
+    vector<int> vec = {-3, 0, 5, 9};
+
+    Node* root = new Node(-10);
+
+    for (auto it : vec) {
+        Node* n = new Node(it);
+        treeInsert(root, n);
+    }
+    vector<string> vals;
+    vector<Node*> childs;
+    childs.push_back(root);
+    levelOrderWithNull(childs, vals);
+
+    for (auto it : vals) {
+        cout << it << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
-
+	
+	test1();
     std::cout << "main exit\n";
     return 0;
 }
