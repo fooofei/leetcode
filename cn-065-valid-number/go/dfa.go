faaea1f2ecfe56ab4d9d1532e43c2e2dacf946b9
fooofei <aihujianfei@gmail.com>
2019-11-09 16:04:19 +0800

    065

---
+++ cn-065-valid-number/go/dfa.go
@@ -0,0 +1,20 @@
+package  leetcode
+
+// 参考学习
+// 【编译原理】把正规式转化为DFA
+// https://blog.csdn.net/C2681595858/article/details/85262797
+
+// 不使用 DFA ，朴素校验
+// https://github.com/begeekmyfriend/leetcode/blob/master/065_valid_number/valid_number.c
+
+// Valid Number - By DFA  讲的还欠缺点
+// https://powersnail.com/2016/valid-number/
+
+// valid number 判断字符串是否为有效数字 我觉得讲的还是不够
+// https://www.cnblogs.com/higerzhang/p/4086191.html
+
+
+func isNumber(s string) bool {
+
+	return false
+}
---
+++ cn-065-valid-number/go/dfa_test.go
@@ -0,0 +1,9 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func TestIsNumber(t *testing.T) {
+
+}
