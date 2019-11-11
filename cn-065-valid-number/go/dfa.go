f345c5f9bbe148ff5d36f9d80c2b16375ea32a57
h00437043 <hujianfei3@huawei.com>
2019-11-11 11:44:56 +0800

    waiting for test

--- cn-065-valid-number/go/dfa.go
+++ cn-065-valid-number/go/dfa.go
@@ -22,7 +22,72 @@ package leetcode
 // 他自己的积累 让他有了轻功
 // https://zhuanlan.zhihu.com/p/25879478
 
-func isNumber(s string) bool {
+// 总结的好，从这里知道符合题意的正则表达式怎么写
+// https://leetcode-cn.com/problems/valid-number/solution/yong-zheng-ze-biao-da-shi-jie-jue-you-xiao-shu-zi-/
 
-	return false
+// 065 https://leetcode-cn.com/problems/valid-number/
+
+
+// 解题步骤
+// 1 看懂题目，找到怎么写符合题意的正则表达式
+// 2 根据正规式组成单元，把正则表达式画出 NFA 也叫 Thompson 构造法
+// 3 把 NFA 转为 DFA， 画出 DFA 图之前，应该有矩阵，有了矩阵就能直接使用表驱动法解题
+
+
+func toMove(c byte) int {
+	switch c {
+	case ' ':
+		return 0
+	case '+','-':
+		return 1
+	case '0','1','2','3','4','5','6','7','8','9':
+		return 2
+	case '.':
+		return 3
+	case 'e','E':
+		return 4
+	default:
+		return 5
+	}
+}
+
+// 从 NFA 得到的 DFA 表中 转为表驱动法解决 // 未经过化简
+func makeMatrix() [][]int {
+
+	m := [][]int {
+		{ 1, 2, 3, 4,-1,-1}, // 0
+		{-1, 2, 3, 4,-1,-1}, // 1
+		{-1,-1, 3, 4,-1,-1}, // 2
+		{ 5,-1, 6, 7, 8,-1}, // 3
+		{-1,-1, 9,-1,-1,-1}, // 4
+		{ 5,-1,-1,-1,-1,-1}, // 5
+		{ 5,-1, 6, 7, 8,-1}, // 6
+		{ 5,-1, 9,-1, 8,-1}, // 7
+		{-1,10,11,-1,-1,-1}, // 8
+		{ 5,-1, 9,-1, 8,-1}, // 9
+		{-1,-1,11,-1,-1,-1}, // 10
+		{ 5,-1,12,-1,-1,-1}, // 11
+		{ 5,-1,12,-1,-1,-1}, // 12
+	}
+	return m
+}
+
+func isNumber(s string) bool {
+	state := 0
+
+	bs := []byte(s)
+	matrix := makeMatrix()
+
+	for _,c := range bs {
+		move := toMove(c)
+		state = matrix[state][move]
+	}
+
+	// 根据矩阵表，得到的结束状态为
+	switch state {
+	case 3,5,6,7,9,11,12:
+		return true
+	default:
+		return false
+	}
 }
