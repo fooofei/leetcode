edbc7efd076afd1c005b735bf8bdc5db3d3518f2
fooofei <aihujianfei@gmail.com>
2019-11-12 10:27:28 +0800

    add 235

---
+++ cn-235-lowest-common-ancestor-of-binary-tree/go/code.go
@@ -0,0 +1,13 @@
+package leetcode
+
+// 235 https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	return nil
+}
