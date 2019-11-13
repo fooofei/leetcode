e57668e0d672529f32d77e781d93e8e834435a96
fooofei <aihujianfei@gmail.com>
2019-11-13 11:08:18 +0800

    28 for dfa solution

--- cn-028-impl-strstr/go/code.go
+++ cn-028-impl-strstr/go/code.go
@@ -1,28 +1,37 @@
 package code
 
-func buildDFA(pattern []byte) [][]int {
+// 028 https://leetcode-cn.com/problems/implement-strstr/
 
-	uniqCount := 0
+// 这个讲的挺多，我还是看不懂
+// https://www.tuhd.top/2017/06/29/2017-06-29-5%E5%AD%97%E7%AC%A6%E4%B8%B25-3%E5%AD%90%E5%AD%97%E7%AC%A6%E4%B8%B2%E6%9F%A5%E6%89%BE/
+
+// 看不懂的点：构造出所有状态和合法的输入产生的转移，这个是一步到位的，
+// 但是对于每个状态中非法的输入，该怎么转移状态 如何构造呢？
+
+func buildDFA(pattern []byte) [][]int {
 	// 尽量使用多语言共有的数据结构 这里不使用 map
+	// 同样的算法也能快速应用到其他语言
 	uniqMap := make([]int, 0x100)
+	uniqChars := make([]byte, 0)
 	for i := 0; i < len(pattern); i++ {
 		c := pattern[i]
 		if uniqMap[c] < 1 {
-			uniqCount += 1
-			uniqMap[c] = uniqCount
+			uniqChars = append(uniqChars, c)
+			uniqMap[c] = len(uniqChars)
 		}
 	}
 	matrix := make([][]int, 0x100)
 	for i := range matrix {
 		matrix[i] = make([]int, len(pattern))
 	}
-	matrix[pattern[0]][0] = 1
 
+	matrix[pattern[0]][0] = 1
 	x := 0 // LPS (longest prefix suffix)
-	for j := 0; j < len(pattern); j++ {
+	for j := 1; j < len(pattern); j++ {
 		c := pattern[j]
-		for k := 0; k < uniqCount; k++ {
-			matrix[k][j] = matrix[k][x]
+		for k := range uniqChars {
+			uc := uniqChars[k]
+			matrix[uc][j] = matrix[uc][x]
 		}
 		matrix[c][j] = j + 1
 		x = matrix[c][x]
@@ -43,15 +52,16 @@ func strStr(str string, pattern string) int {
 	j := 0
 	for i := 0; i < len(strBytes); i++ {
 		c := strBytes[i]
-		if c == patternBytes[j] {
-			j++
-		} else {
-			j = matrix[c][j]
-		}
+		j = matrix[c][j]
+		// 不需要写成这样
+		//if c == patternBytes[j] {
+		//	j++
+		//} else {
+		//	j = matrix[c][j]
+		//}
 		if j >= len(patternBytes) {
 			return i - len(patternBytes) + 1
 		}
 	}
-
 	return -1
 }
--- cn-028-impl-strstr/go/code_test.go
+++ cn-028-impl-strstr/go/code_test.go
@@ -6,17 +6,20 @@ import "gotest.tools/assert"
 func Test1(t *testing.T) {
 	ret := strStr("hello", "ll")
 	assert.Equal(t, ret, 2)
-
 }
 
 func Test2(t *testing.T) {
 	ret := strStr("aaaaa", "bba")
 	assert.Equal(t, ret, -1)
-
 }
 
 func Test3(t *testing.T) {
 	ret := strStr("mississippi", "issip")
 	assert.Equal(t, ret, 4)
+}
 
+func Test4(t *testing.T) {
+	input := []byte("ababac")
+	_ = buildDFA(input)
+	assert.Equal(t,true,true)
 }
