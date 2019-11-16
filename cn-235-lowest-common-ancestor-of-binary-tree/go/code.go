be7b75c164ede6a901369555426dbbe5acc94e22
fooofei <aihujianfei@gmail.com>
2019-11-16 11:05:31 +0800

    add 235

--- cn-235-lowest-common-ancestor-of-binary-tree/go/code.go
+++ cn-235-lowest-common-ancestor-of-binary-tree/go/code.go
@@ -8,6 +8,46 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 遍历 TREE 得到包含某结点在内的和它的父亲结点
+// 组成一个 list
+// 顺序是 自己 自己.parent 自己.parent.parent  ...
+func getParentLists(root, target *TreeNode) []*TreeNode {
+	if root == nil {
+		return []*TreeNode{}
+	}
+	if root == target {
+		return []*TreeNode{root}
+	}
+	left := getParentLists(root.Left, target)
+	if len(left) > 0 {
+		return append(left, root)
+	}
+	right := getParentLists(root.Right, target)
+	if len(right) > 0 {
+		return append(right, root)
+	}
+	return []*TreeNode{}
+}
+
+// 问题求解就被转化为 // 160  https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+func findCommon(pList, qList []*TreeNode) *TreeNode {
+	var i, j int
+	pList = append(pList, qList...)
+	qList = append(qList, pList...)
+	for pList[i] != qList[j] {
+		i += 1
+		j += 1
+	}
+	return pList[i]
+}
+
+// 我这样做没有利用到 二叉搜索树的性质
+// 我把它转化为两个 list ，求两个 list 的第一个交叉的结点
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	return nil
+	if root == nil {
+		return nil
+	}
+	pList := getParentLists(root, p)
+	qList := getParentLists(root, q)
+	return findCommon(pList, qList)
 }
--- cn-235-lowest-common-ancestor-of-binary-tree/go/code_test.go
+++ cn-235-lowest-common-ancestor-of-binary-tree/go/code_test.go
@@ -14,5 +14,60 @@ func assertEqualInts(t *testing.T, a []int, b []int) {
 }
 
 func Test1(t *testing.T) {
+	n1 := &TreeNode{
+		Val:   6,
+		Left:  nil,
+		Right: nil,
+	}
+	n2 := &TreeNode{
+		Val:   2,
+		Left:  nil,
+		Right: nil,
+	}
+	n3 := &TreeNode{
+		Val:   8,
+		Left:  nil,
+		Right: nil,
+	}
+	n4 := &TreeNode{
+		Val:   0,
+		Left:  nil,
+		Right: nil,
+	}
+	n5 := &TreeNode{
+		Val:   4,
+		Left:  nil,
+		Right: nil,
+	}
+	n6 := &TreeNode{
+		Val:   7,
+		Left:  nil,
+		Right: nil,
+	}
+	n7 := &TreeNode{
+		Val:   9,
+		Left:  nil,
+		Right: nil,
+	}
+	n8 := &TreeNode{
+		Val:   3,
+		Left:  nil,
+		Right: nil,
+	}
+	n9 := &TreeNode{
+		Val:   5,
+		Left:  nil,
+		Right: nil,
+	}
+	n5.Left = n8
+	n5.Right = n9
+	n2.Left = n4
+	n2.Right = n5
+	n3.Left = n6
+	n3.Right = n7
+	n1.Left = n2
+	n1.Right = n3
 
+	ret := lowestCommonAncestor(n1, n2, n3)
+	assert.Equal(t, ret, n1)
 }
