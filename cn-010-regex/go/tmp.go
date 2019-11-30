e540b8b47afc22f78c3edfb242d82f2cbbe3864c
h00437043 <hujianfei3@huawei.com>
2019-11-30 18:42:05 +0800

    nfa for regex

--- cn-010-regex/go/tmp.go
+++ cn-010-regex/go/tmp.go
@@ -1,147 +1,161 @@
 package main
 
-import (
-	"fmt"
-	_ "rsc.io/binaryregexp"
-	"strings"
-)
+// 010 https://leetcode-cn.com/problems/regular-expression-matching/
 
+// Russ Cox 的博客的 Golang 语言实现
+// https://medium.com/@phanindramoganti/regex-under-the-hood-implementing-a-simple-regex-compiler-in-go-ef2af5c6079
+// https://github.com/phanix5/Simple-Regex-Complier-in-Go/blob/master/regex.go
+
+// Russ Cox 的博客
 // Regular Expression Matching Can Be Simple And Fast
 // (but is slow in Java, Perl, PHP, Python, Ruby, ...)
 // https://swtch.com/~rsc/regexp/regexp1.html
-// https://leetcode.com/problems/regular-expression-matching/discuss/5951/C-solution-based-on-NFA-without-backtracking-only-4ms.-From-Russ-Cox.
+// Russ Cox 说这是 Golang 的实现
 // https://godoc.org/rsc.io/binaryregexp
+
+// 题目解答 Ross Cox 的方案
 // https://leetcode.com/problems/regular-expression-matching/discuss/393324/State-machine-solution-easy-to-follow
 
-// 这里一个版本
-// https://mikecoder.cn/post/122/
+// 题目解答 Ross Cox 的方案
+// https://leetcode.com/problems/regular-expression-matching/discuss/5951/C-solution-based-on-NFA-without-backtracking-only-4ms.-From-Russ-Cox.
+//
 
-// 来自 https://leetcode-cn.com/problems/regular-expression-matching/solution/yi-bu-dao-wei-zhi-jie-an-zheng-ze-biao-da-shi-de-s/
-// 没有好感 没继续研究
+// 中国人的翻译正则表达式和 NFA - luobo tang的文章 - 知乎
+// https://zhuanlan.zhihu.com/p/45954880
 
-func debug(v ...interface{}) {
-	//log.Println(v...)
-}
+// 缺点，原版本不支持 regex 中的 . 匹配，我根据 leetcode 解答学习过来
 
-func toString(i interface{}) string {
-	switch i.(type) {
-	case int:
-		return fmt.Sprintf("%v", i)
-	case string:
-		return fmt.Sprintf("%v", i)
-	case bool:
-		return fmt.Sprintf("%v", i)
+// Ken Thompson's multi-state simulation
+
+type StateType int
+
+const (
+	Default StateType = 0 // out used
+	Split   StateType = 1 // out & out1 used
+	Match   StateType = 2
+)
+
+func (t StateType) String() string {
+	switch t {
+	case Split:
+		return "Split"
+	case Match:
+		return "Match"
 	default:
-		return fmt.Sprintf("%p", i)
+		return "Default"
 	}
 }
 
-func isMatch(s string, p string) bool {
-	begin := new(Node)
-	begin.C = '>'
-	begin.Size = generatePattern(begin, p)
-	debug(begin.String())
-	return check(begin, s, 0)
+type State struct {
+	Type StateType // type identifier
+	Ch   rune      // char
+	Out  *State
+	Out1 *State
+	ListID int
 }
 
-type Node struct {
-	C        byte
-	Parent   *Node
-	Children map[byte][]*Node
-	End      bool
-	Size     int
-}
-
-func (n *Node) String() string {
-	return n.StringLevel(0, make(map[*Node]bool))
-}
-
-func (n *Node) StringLevel(level int, finishNodes map[*Node]bool) string {
-	r := make([]string, 0)
-	if n.End {
-		r = append(r, fmt.Sprintf("  id%s{%v};", toString(n), string(n.C)))
+func addState(l []*State, s *State, listid int) []*State {
+	if s.ListID == listid {
+		return l
+	}
+	if s.Type == Split {
+		l = addState(l, s.Out, listid)
+		l = addState(l, s.Out1, listid)
 	} else {
-		r = append(r, fmt.Sprintf("  id%s(%v);", toString(n), string(n.C)))
+		l = append(l, s)
 	}
-	finishNodes[n] = true
-	for k, v := range n.Children {
-		for _, c := range v {
-			if _, ok := finishNodes[c]; !ok {
-				r = append(r, c.StringLevel(level+1, finishNodes))
-			}
-			r = append(r, fmt.Sprintf("  id%s -- %s --> id%s;", toString(n), string(k), toString(c)))
-		}
-	}
-	return strings.Join(r, "\n")
+	return l
 }
 
-func (n *Node) Append(child *Node) {
-	m := n.Children
-	if m == nil {
-		m = make(map[byte][]*Node)
-		n.Children = m
-	}
-	list := m[child.C]
-	if list == nil {
-		list = make([]*Node, 0)
-	}
-	for _, v := range list {
-		if v == child {
-			return
+func step(clist []*State, ch rune, nlist []*State, listid * int) []*State {
+	(*listid) += 1
+	for _, s := range clist {
+		if s.Ch == '.' && s.Out != nil {
+			nlist = addState(nlist, s.Out, *listid)
+		} else if ch == s.Ch && s.Out != nil {
+			nlist = addState(nlist, s.Out, *listid)
 		}
 	}
-	list = append(list, child)
-	m[child.C] = list
+	return nlist
 }
 
-func generatePattern(parent *Node, str string) int {
-	if len(str) <= 0 {
-		parent.End = true
-		return parent.Size
+func makeState(ch, nextch rune) (*State, int) {
+	if nextch == '*' {
+		split := &State{
+			Type: Split, Ch: 0,
+		}
+		s := &State{
+			Type: Default, Ch: ch,
+		}
+		split.Out1 = s
+		s.Out = split
+		return split, 2
+	} else {
+		s := &State{
+			Type: Default, Ch: ch,
+		}
+		return s, 1
 	}
-	vnow := parent
-	switch str[0] {
-	case '*':
-		parent.Size = 0
-		parent.Append(parent)
-	default:
-		node := new(Node)
-		node.C = str[0]
-		parent.Append(node)
-		node.Parent = parent
-		node.Size = 1
-		vnow = node
+}
+
+func build(runep []rune, matchState *State) *State {
+	var start *State
+	start = &State{
+		Type: Default, Ch: 0,
 	}
-	ret := generatePattern(vnow, str[1:])
-	if ret == 0 {
-		parent.End = true
+	i := 0
+	if len(runep) > 1 {
+		start, i = makeState(runep[0], runep[1])
+	} else if len(runep) > 0 {
+		start, i = makeState(runep[0], '$')
 	}
-	addParent := parent
-	for addParent.Parent != nil {
-		if addParent.Size == 0 {
-			debug(toString(vnow), " -> ", toString(addParent.Parent))
-			addParent.Parent.Append(vnow)
-			addParent = addParent.Parent
+	curState := start
+	for i < len(runep) {
+		ch := runep[i]
+		var nextch rune
+		if i+1 >= len(runep) {
+			nextch = '$'
 		} else {
-			break
+			nextch = runep[i+1]
 		}
+		newState, off := makeState(ch, nextch)
+		curState.Out = newState
+		curState = newState
+		i += off
 	}
-	return parent.Size + ret
+	curState.Out = matchState
+	return start
 }
 
-func check(now *Node, str string, idx int) bool {
-	if len(str) <= idx {
-		return now.End
-	}
-	list := now.Children['.']
-	for _, v := range now.Children[str[idx]] {
-		list = append(list, v)
-	}
-	for _, v := range list {
-		r := check(v, str, idx+1)
-		if r {
+func hasMatch(l []*State, matchState *State) bool {
+	for _, s := range l {
+		if s == matchState {
 			return true
 		}
 	}
 	return false
+}
+
+func isMatch(s string, p string) bool {
+	if len(p) == 0 {
+		return len(s) == 0
+	}
+	matchState := &State{
+		Type: Match,
+	}
+	runep := []rune(p)
+	start := build(runep, matchState)
+
+	clist := make([]*State, 0, len(runep)+1)
+	nlist := make([]*State, 0, len(runep)+1)
+
+	// 原版本的称呼，作用是减少 clist 中重复的状态
+	listid := 1
+	clist = addState(clist, start, listid)
+	for _, ch := range s {
+		nlist = nlist[:0]
+		nlist = step(clist, ch, nlist)
+		clist, nlist = nlist, clist
+	}
+	return hasMatch(clist, matchState)
 }
--- cn-010-regex/go/tmp_test.go
+++ cn-010-regex/go/tmp_test.go
@@ -6,31 +6,29 @@ import (
 )
 
 func Test1(t *testing.T) {
-
 	assert.Equal(t, isMatch("", "c*c*"), true)
 }
 
 func Test2(t *testing.T) {
-
 	assert.Equal(t, isMatch("a", "ab*"), true)
 }
 
 func Test3(t *testing.T) {
-
 	assert.Equal(t, isMatch("aaa", "a*a"), true)
 }
 
 func Test4(t *testing.T) {
-
 	assert.Equal(t, isMatch("ab", ".*c"), false)
 }
 
 func Test5(t *testing.T) {
-
 	assert.Equal(t, isMatch("ab", ".*"), true)
 }
 
 func Test6(t *testing.T) {
-
 	assert.Equal(t, isMatch("mississippi", "mis*is*p*."), false)
+}
+
+func Test7(t *testing.T) {
+	assert.Equal(t, isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c"), false)
 }
