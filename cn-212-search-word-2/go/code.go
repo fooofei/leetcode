d0947caa26988fa547fddecfad7841001946fe1d
fooofei <aihujianfei@gmail.com>
2019-12-02 21:25:00 +0800

    212

--- cn-212-search-word-2/go/code.go
+++ cn-212-search-word-2/go/code.go
@@ -2,39 +2,23 @@ package leetcode
 
 // 212 https://leetcode-cn.com/problems/word-search-ii/
 
+// 用不到 AC自动机
+// 题意需要反复理解：是从任意点开始向上下左右方向步进
+
 // a-z
 const CHILDS_MAX_SIZE = 27
 
 type Node struct {
-	C        byte
-	Parent   *Node
-	NextMap  [CHILDS_MAX_SIZE]*Node
-	NextList []*Node
-	Fail     *Node
-	Ended    bool
+	C         byte
+	Parent    *Node
+	NextMap   [CHILDS_MAX_SIZE]*Node
+	NextList  []*Node
+	Ended     bool
+	SubString []byte
 }
 
-type Trie struct {
-	Root   *Node
-	StrCnt int
-}
-
-func reverse(s []byte) []byte {
-	if len(s) < 2 {
-		return s
-	}
-	i := 0
-	j := len(s) - 1
-	for i < j {
-		s[i], s[j] = s[j], s[i]
-		i++
-		j--
-	}
-	return s
-}
-
-func (t *Trie) add(word []byte) {
-	p := t.Root
+func addWord(root *Node, word []byte) {
+	p := root
 	for _, c := range word {
 		idx := c - 'a'
 		if p.NextMap[idx] == nil {
@@ -50,93 +34,61 @@ func (t *Trie) add(word []byte) {
 		p = p.NextMap[idx]
 	}
 	p.Ended = true
-	t.StrCnt += 1
+	p.SubString = append([]byte{}, word...)
 }
 
-func (t *Trie) build() {
-	queue := make([]*Node, 0)
-	root := t.Root
-	for _, n := range root.NextList {
-		n.Fail = root
-		queue = append(queue, n)
-	}
-	for len(queue) > 0 {
-		parent := queue[0]
-		queue = queue[1:]
-		for _, p := range parent.NextList {
-			fail := parent.Fail
-			idx := p.C - 'a'
-			for fail != nil {
-				if fail.NextMap[idx] != nil {
-					p.Fail = fail.NextMap[idx]
-					break
-				}
-				fail = fail.Fail
-			}
-			if fail == nil {
-				p.Fail = root
-			}
-			queue = append(queue, p)
-		}
-	}
-}
+// 抛弃我自己的没拐弯的解决方案了，原来测试用例是可能拐弯的
+// 学习了这里的解决方案 https://leetcode-cn.com/problems/word-search-ii/solution/cqian-zhui-shu-ju-ta-ma-rong-yi-dong-by-chashao/
+// 连 Fail 指针都用不上
 
-func traverse(root *Node, node *Node, board [][]byte, i, j int, visited [][]bool) []string {
+func traverse(node *Node, board [][]byte, i, j int) []string {
 	ret := make([]string, 0)
-	if node.Ended {
-		n := node
-		s := make([]byte, 0)
-		for n != root {
-			s = append(s, n.C)
-			n = n.Parent
-		}
-		ret = append(ret, string(reverse(s)))
-	}
-	if i >= len(board) {
+	if i >= len(board) || i < 0 {
 		return ret
 	}
-	if j >= len(board[0]) {
+	if j >= len(board[0]) || j < 0 {
 		return ret
 	}
 	c := board[i][j]
 	idx := c - 'a'
-	for node.NextMap[idx] == nil && node != root {
-		node = node.Fail
-	}
-	if node == root {
-		if visited[i][j] {
-			return ret
-		}
-		visited[i][j] = true
+	if node.NextMap[idx] == nil {
+		return ret
 	}
 	node = node.NextMap[idx]
-	if node == nil {
-		node = root
+	if node.Ended {
+		ret = append(ret, string(node.SubString))
+		node.Ended = false // 标记为使用过了，这样不会重复，巧妙啊
 	}
-	ret1 := traverse(root, node, board, i, j+1, visited)
+	// 错误点：矩阵里的字符不能重复使用
+	// 置换为其他字符后，就表示遍历过了
+	board[i][j] = '{' // = 'z'+1
+	ret1 := traverse(node, board, i, j+1)
 	ret = append(ret, ret1...)
-	ret2 := traverse(root, node, board, i+1, j, visited)
-	ret = append(ret, ret2...)
+	ret1 = traverse(node, board, i, j-1)
+	ret = append(ret, ret1...)
+	ret1 = traverse(node, board, i+1, j)
+	ret = append(ret, ret1...)
+	ret1 = traverse(node, board, i-1, j)
+	ret = append(ret, ret1...)
+	board[i][j] = c
 	return ret
 }
 
 func findWords(board [][]byte, words []string) []string {
-	trie := &Trie{
-		Root: &Node{
-			NextMap:  [CHILDS_MAX_SIZE]*Node{},
-			NextList: make([]*Node, 0),
-		},
+	root := &Node{
+		NextMap:  [CHILDS_MAX_SIZE]*Node{},
+		NextList: make([]*Node, 0),
 	}
 	for _, w := range words {
 		wbs := []byte(w)
-		trie.add(wbs)
-		trie.add(reverse(wbs))
+		addWord(root, wbs)
 	}
-	trie.build()
-	// 以某点为起始点 是否查询过
-	visited := make([][]bool, len(board))
-	for i := range visited {
-		visited[i] = make([]bool, len(board[0]))
+	ret := make([]string, 0)
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[i]); j++ {
+			ret1 := traverse(root, board, i, j)
+			ret = append(ret, ret1...)
+		}
 	}
-	return traverse(trie.Root, trie.Root, board, 0, 0, visited)
+	return ret
 }
--- cn-212-search-word-2/go/code_test.go
+++ cn-212-search-word-2/go/code_test.go
@@ -1,31 +1,53 @@
 package leetcode
 
 import (
+	"sort"
 	"testing"
 
 	"gotest.tools/assert"
 )
 
 func assertEqualInts(t *testing.T, a []int, b []int) {
-	assert.Equal(t, len(a), len(b))
+	assert.Equal(t, len(a), len(b), "%v != %v", a, b)
 	for i := 0; i < len(a); i++ {
 		assert.Equal(t, a[i], b[i])
 	}
 }
 
 func assertEqualStrings(t *testing.T, a []string, b []string) {
-	assert.Equal(t, len(a), len(b))
+	assert.Equal(t, len(a), len(b), "%v != %v", a, b)
 	for i := 0; i < len(a); i++ {
 		assert.Equal(t, a[i], b[i])
 	}
 }
 
 func Test1(t *testing.T) {
-	assertEqualStrings(t, findWords([][]byte{
+	ret := findWords([][]byte{
 		{'o', 'a', 'a', 'n'},
 		{'e', 't', 'a', 'e'},
 		{'i', 'h', 'k', 'r'},
 		{'i', 'f', 'l', 'v'},
-	},[]string{"oath", "pea", "eat", "rain"}),
-		[]string{"eat", "oath"})
+	}, []string{"oath", "pea", "eat", "rain"})
+	sort.Strings(ret)
+	expect := []string{"eat", "oath"}
+	sort.Strings(expect)
+	assertEqualStrings(t, ret, expect)
+}
+
+// 竟然有拐弯的测试用例
+func Test2(t *testing.T) {
+	assertEqualStrings(t, findWords([][]byte{
+		{'a', 'b'},
+		{'c', 'd'},
+	}, []string{"acdb"}),
+		[]string{"acdb"})
+}
+
+func Test3(t *testing.T) {
+	ret := findWords([][]byte{
+		{'a', 'a'},
+	}, []string{"aaa"})
+	sort.Strings(ret)
+	expect := []string{}
+	assertEqualStrings(t, ret, expect)
 }
